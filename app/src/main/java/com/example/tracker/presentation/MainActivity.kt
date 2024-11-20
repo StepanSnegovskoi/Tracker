a6eebefa7f5d2de9419a99366181183956a62920
stepann <stepan.snegovskoi@mail.ru>
2024-11-20 19:06:54 +0300

    Удалены тесты для бд

--- app/src/main/java/com/example/tracker/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tracker/presentation/MainActivity.kt
@@ -24,20 +24,6 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    private val addCard by lazy {
-        AddCard(CardRepositoryImpl(application))
-    }
-
-    private val deleteCard by lazy {
-        DeleteCard(CardRepositoryImpl(application))
-    }
-    private val getCard by lazy {
-        GetCard(CardRepositoryImpl(application))
-    }
-    private val getAllCards by lazy {
-        GetAllCards(CardRepositoryImpl(application))
-    }
-
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -52,93 +38,9 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.buttonGetCard.setOnClickListener {
-            val id = binding.editTextId.text.toString().toInt()
-            lifecycleScope.launch {
-                withContext(Dispatchers.IO) {
-                    val idContains = getAllCards().map { it.id }.contains(id)
-                    when (idContains) {
-
-                        true -> {
-                            val cardString = getCard(binding.editTextId.text.toString().toInt()).toString()
-                            withContext(Dispatchers.Main) {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    cardString,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-
-                        false -> {
-                            withContext(Dispatchers.Main) {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    "Элемента с таким айди в базе данных нету",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        binding.buttonAddCardToDb.setOnClickListener {
-            lifecycleScope.launch {
-                withContext(Dispatchers.IO) {
-                    addCard(
-                        Card(
-                            name = "name",
-                            description = "description",
-                            deadline = 1
-                        )
-                    )
-                }
-            }
-        }
-
-        binding.buttonGetAllCards.setOnClickListener {
-            lifecycleScope.launch {
-                withContext(Dispatchers.IO) {
-                    Log.d(TAG, getAllCards().toString())
-                    val cardsString = getAllCards().toString()
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, cardsString, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-
-        binding.buttonDeleteCardFromDb.setOnClickListener {
-            val id = binding.editTextId.text.toString().toInt()
-            lifecycleScope.launch {
-                withContext(Dispatchers.IO) {
-                    val idContains = getAllCards().map { it.id }.contains(id)
-                    when (idContains) {
-
-                        true -> {
-                            deleteCard(id).toString()
-                        }
-
-                        false -> {
-                            withContext(Dispatchers.Main) {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    "Элемента с таким айди в базе данных нету",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 
     companion object {
         private const val TAG = "MainActivityTestBd"
     }
 }
-
-
