b19a285eeb91f3fbed37aab935f24fb1bd225eda
stepann <stepan.snegovskoi@mail.ru>
2025-02-13 20:17:34 +0300

    Add Gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,9 @@ dependencies {
     implementation(libs.room.ktx)
     ksp(libs.room.compiler)
 
+    // Gson
+    implementation(libs.gson)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
