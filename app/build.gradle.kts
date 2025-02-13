303078b5e3596cc3c52de2856adcb7eae81b71a1
stepann <stepan.snegovskoi@mail.ru>
2025-02-13 20:13:17 +0300

    Add Dagger-core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     implementation(libs.decompose)
     implementation(libs.decompose.ext)
 
+    // Dagger
+    implementation(libs.dagger)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
