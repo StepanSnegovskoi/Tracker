335093f1f96827f3f0cf3ed566778513dc6933cf
stepann <stepan.snegovskoi@mail.ru>
2025-02-16 18:00:46 +0300

    Add isCompleted and addingTime properties to Task

--- app/src/main/java/com/example/trackernew/domain/entity/Task.kt
+++ app/src/main/java/com/example/trackernew/domain/entity/Task.kt
@@ -1,7 +1,11 @@
 package com.example.trackernew.domain.entity
 
+import java.util.Calendar
+
 data class Task(
     val id: Int,
     val name: String,
     val description: String,
+    val isCompleted: Boolean,
+    val addingTime: Calendar
 )
