043fa33b3b30e9c07f8bdf706b3a76ed108a9d03
stepann <stepan.snegovskoi@mail.ru>
2025-03-01 14:29:58 +0300

    Add outlined text field colors

--- app/src/main/java/com/example/trackernew/ui/theme/Color.kt
+++ app/src/main/java/com/example/trackernew/ui/theme/Color.kt
@@ -1,6 +1,21 @@
 package com.example.trackernew.ui.theme
 
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+
+@Composable
+fun getOutlinedTextFieldColors() = OutlinedTextFieldDefaults.colors(
+    disabledBorderColor = Color.Black.copy(0.3f),
+    focusedBorderColor = Color.Black.copy(0.3f),
+    unfocusedBorderColor = Color.Black.copy(0.3f),
+    disabledTextColor = Color.Black,
+    focusedTextColor = Color.Black,
+    unfocusedTextColor = Color.Black,
+    disabledLabelColor = Color.Black.copy(0.7f),
+    focusedLabelColor = Color.Black.copy(0.7f),
+    unfocusedLabelColor = Color.Black.copy(0.7f),
+)
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
