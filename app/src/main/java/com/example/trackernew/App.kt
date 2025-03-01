fac719560fcae9eac9d2c4194fee98f29c197604
stepann <stepan.snegovskoi@mail.ru>
2025-03-01 18:26:42 +0300

    Add component in Application class

--- app/src/main/java/com/example/trackernew/App.kt
+++ app/src/main/java/com/example/trackernew/App.kt
@@ -2,6 +2,9 @@ package com.example.trackernew
 
 import android.app.Application
 import com.example.trackernew.di.ApplicationScope
+import com.example.trackernew.di.DaggerApplicationComponent
 
 @ApplicationScope
-class App : Application()
+class App : Application() {
+    val component = DaggerApplicationComponent.factory().create(this)
+}
