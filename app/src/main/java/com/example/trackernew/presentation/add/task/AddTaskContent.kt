abe68d96bee012ee490693653da566574b361410
stepann <stepan.snegovskoi@mail.ru>
2025-03-01 14:22:16 +0300

    Fix AddTaskContent
    Экран по случайности был настроен для добавления занятия в расписании, переделан на добавление задачи

--- app/src/main/java/com/example/trackernew/presentation/add/task/AddTaskContent.kt
+++ app/src/main/java/com/example/trackernew/presentation/add/task/AddTaskContent.kt
@@ -1,240 +1,104 @@
 package com.example.trackernew.presentation.add.task
 
-import android.util.Log
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-
-val items = listOf("Математика", "КГ", "Диффуры")
 
 @Preview
 @Composable
 fun AddTaskContent() {
-    Box(
+    Scaffold (
         modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Column {
-            OutlinedTextFieldNameWithMenu()
-            OutlinedTextFieldDescriptionWithMenu()
-            OutlinedTextFieldDeadlineWithMenu()
-        }
-    }
-}
+            .fillMaxSize(),
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
 
-@Composable
-fun OutlinedTextFieldNameWithMenu() {
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-    Menu(
-        expanded = expanded,
-        items = items,
-        onDismissRequest = {
-            expanded.value = false
-        },
-        onItemClick = {
-            expanded.value = false
-        },
-        content = { modifier ->
-            OutlinedTextFieldName(
-                modifier = modifier,
-                onIconClick = {
-                    expanded.value = !expanded.value
                 }
-            )
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
         }
-    )
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            OutlinedTextFieldName {
+
+            }
+            OutlinedTextFieldDescription {
+
+            }
+            OutlinedTextFieldDeadline {
+
+            }
+        }
+    }
 }
 
 @Composable
 fun OutlinedTextFieldName(
-    modifier: Modifier = Modifier,
-    onIconClick: () -> Unit
+    onValueChange: (String) -> Unit
 ) {
     OutlinedTextField(
         modifier = Modifier
-            .fillMaxWidth()
-            .then(modifier),
+            .fillMaxWidth(),
         readOnly = true,
         label = {
             Text(text = "Название")
         },
-        trailingIcon = {
-            Icon(
-                modifier = Modifier
-                    .clickable {
-                        onIconClick()
-                    },
-                imageVector = Icons.Default.KeyboardArrowUp,
-                contentDescription = null,
-            )
-        },
         value = "",
         onValueChange = {
-        }
-    )
-}
-
-@Composable
-fun OutlinedTextFieldDescriptionWithMenu() {
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-    Menu(
-        expanded = expanded,
-        items = items,
-        onDismissRequest = {
-            expanded.value = false
-        },
-        onItemClick = {
-            expanded.value = false
-        },
-        content = { modifier ->
-            OutlinedTextFieldDescription (
-                modifier = modifier,
-                onIconClick = {
-                    expanded.value = !expanded.value
-                }
-            )
+            onValueChange(it)
         }
     )
 }
 
 @Composable
 fun OutlinedTextFieldDescription(
-    modifier: Modifier = Modifier,
-    onIconClick: () -> Unit
+    onValueChange: (String) -> Unit
 ) {
     OutlinedTextField(
         modifier = Modifier
-            .fillMaxWidth()
-            .then(modifier),
+            .fillMaxWidth(),
         readOnly = true,
         label = {
             Text(text = "Описание")
         },
-        trailingIcon = {
-            Icon(
-                modifier = Modifier
-                    .clickable {
-                        onIconClick()
-                    },
-                imageVector = Icons.Default.KeyboardArrowUp,
-                contentDescription = null,
-            )
-        },
         value = "",
         onValueChange = {
-        }
-    )
-}
-
-@Composable
-fun OutlinedTextFieldDeadlineWithMenu() {
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-    Menu(
-        expanded = expanded,
-        items = items,
-        onDismissRequest = {
-            expanded.value = false
-        },
-        onItemClick = {
-            expanded.value = false
-        },
-        content = { modifier ->
-            OutlinedTextFieldDeadline (
-                modifier = modifier,
-                onIconClick = {
-                    expanded.value = !expanded.value
-                }
-            )
+            onValueChange(it)
         }
     )
 }
 
 @Composable
 fun OutlinedTextFieldDeadline(
-    modifier: Modifier = Modifier,
-    onIconClick: () -> Unit
+    onValueChange: (String) -> Unit
 ) {
     OutlinedTextField(
         modifier = Modifier
-            .fillMaxWidth()
-            .then(modifier),
+            .fillMaxWidth(),
         readOnly = true,
         label = {
             Text(text = "Дедлайн")
         },
-        trailingIcon = {
-            Icon(
-                modifier = Modifier
-                    .clickable {
-                        onIconClick()
-                    },
-                imageVector = Icons.Default.KeyboardArrowUp,
-                contentDescription = null,
-            )
-        },
         value = "",
         onValueChange = {
+            onValueChange(it)
         }
     )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Menu(
-    expanded: State<Boolean>,
-    items: List<String>,
-    onDismissRequest: () -> Unit,
-    onItemClick: () -> Unit,
-    content: @Composable (Modifier) -> Unit
-) {
-    ExposedDropdownMenuBox(
-        expanded = expanded.value,
-        onExpandedChange = {
-        }
-    ) {
-        content(Modifier.menuAnchor())
-
-        DropdownMenu(
-            modifier = Modifier
-                .fillMaxWidth(),
-            expanded = expanded.value,
-            onDismissRequest = {
-                onDismissRequest()
-            }
-        ) {
-            items.forEach {
-                DropdownMenuItem(
-                    text = {
-                        Text(text = it)
-                    },
-                    onClick = {
-                        onItemClick()
-                    }
-                )
-            }
-        }
-    }
 }
