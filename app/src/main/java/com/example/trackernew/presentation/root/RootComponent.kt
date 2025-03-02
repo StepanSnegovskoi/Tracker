5b72ae2744ff578a98cfea49d82f30d20616f85c
stepann <stepan.snegovskoi@mail.ru>
2025-03-02 18:24:06 +0300

    Create Tasks Config

--- app/src/main/java/com/example/trackernew/presentation/root/RootComponent.kt
+++ app/src/main/java/com/example/trackernew/presentation/root/RootComponent.kt
@@ -3,6 +3,7 @@ package com.example.trackernew.presentation.root
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 import com.example.trackernew.presentation.add.task.AddTaskComponent
+import com.example.trackernew.presentation.tasks.TasksComponent
 
 interface RootComponent {
 
@@ -11,5 +12,7 @@ interface RootComponent {
     sealed interface Child {
 
         data class AddTask(val component: AddTaskComponent) : Child
+
+        data class Tasks(val component: TasksComponent) : Child
     }
 }
