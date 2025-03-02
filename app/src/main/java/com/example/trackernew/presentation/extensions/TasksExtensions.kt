c58613d45d853e5e12fed0ecd57337654237cccd
stepann <stepan.snegovskoi@mail.ru>
2025-03-02 13:47:19 +0300

    Update toDateString method

--- app/src/main/java/com/example/trackernew/presentation/extensions/TasksExtensions.kt
+++ app/src/main/java/com/example/trackernew/presentation/extensions/TasksExtensions.kt
@@ -5,5 +5,9 @@ import java.util.Calendar
 import java.util.Locale
 
 fun Long.toDateString(): String {
-    return SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(this).toString()
+    if (this == 0L) return ""
+
+    return SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
+        .format(this)
+        .toString()
 }
