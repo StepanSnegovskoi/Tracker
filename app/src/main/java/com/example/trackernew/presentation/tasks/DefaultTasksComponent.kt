13aaea821c792a284a8ce93572b7d73268eef049
stepann <stepan.snegovskoi@mail.ru>
2025-03-02 18:25:37 +0300

    Create DefaultTasksComponent

---
+++ app/src/main/java/com/example/trackernew/presentation/tasks/DefaultTasksComponent.kt
@@ -0,0 +1,45 @@
+package com.example.trackernew.presentation.tasks
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import com.arkivanov.mvikotlin.extensions.coroutines.labels
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
+import com.example.trackernew.presentation.extensions.componentScope
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class DefaultTasksComponent @AssistedInject constructor(
+    private val tasksStoreFactory: TasksStoreFactory,
+    @Assisted("componentContext") componentContext: ComponentContext,
+    @Assisted("onAddClick") private val onAddClick: () -> Unit
+) : TasksComponent, ComponentContext by componentContext {
+
+    val store = instanceKeeper.getStore { tasksStoreFactory.create() }
+
+    init {
+        store.labels.onEach {
+            onAddClick()
+        }.launchIn(componentScope())
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val model: StateFlow<TasksStore.State> = store.stateFlow
+
+    override fun onAddClicked() {
+        store.accept(TasksStore.Intent.ClickAdd)
+    }
+
+    @AssistedFactory
+    interface Factory {
+
+        fun create(
+            @Assisted("componentContext") componentContext: ComponentContext,
+            @Assisted("onAddClick") onAddClick: () -> Unit
+        ): DefaultTasksComponent
+    }
+}
