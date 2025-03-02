0a28d1bc859421bf5af794922bb483b72948a3a3
stepann <stepan.snegovskoi@mail.ru>
2025-03-02 13:49:20 +0300

    Create RootContent

---
+++ app/src/main/java/com/example/trackernew/presentation/root/RootContent.kt
@@ -0,0 +1,28 @@
+package com.example.trackernew.presentation.root
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.extensions.compose.stack.Children
+import com.example.trackernew.presentation.add.task.AddTaskContent
+import com.example.trackernew.ui.theme.TrackerNewTheme
+
+@Composable
+fun RootContent(component: RootComponent) {
+    TrackerNewTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Children(stack = component.stack) {
+                when (val instance = it.instance) {
+                    is RootComponent.Child.AddTask -> {
+                        AddTaskContent(instance.component)
+                    }
+                }
+            }
+        }
+    }
+
+}
