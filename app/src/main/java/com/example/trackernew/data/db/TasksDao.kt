c6095671c43ca66f353635cbc873688c82529895
stepann <stepan.snegovskoi@mail.ru>
2025-03-02 18:23:23 +0300

    Add getTasks method to dao

--- app/src/main/java/com/example/trackernew/data/db/TasksDao.kt
+++ app/src/main/java/com/example/trackernew/data/db/TasksDao.kt
@@ -2,11 +2,17 @@ package com.example.trackernew.data.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.trackernew.data.entity.TaskDbModel
+import com.example.trackernew.domain.entity.Task
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TasksDao {
 
     @Insert
     suspend fun saveTask(taskDbModel: TaskDbModel)
+
+    @Query("SELECT * FROM tasks")
+    fun getTasks(): Flow<List<TaskDbModel>>
 }
