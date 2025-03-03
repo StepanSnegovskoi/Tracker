bb12b3794bff2e99e3ed43ca60813d7fc2834820
stepann <stepan.snegovskoi@mail.ru>
2025-03-03 21:37:13 +0300

    Add AddCategory class to Child

--- app/src/main/java/com/example/trackernew/presentation/root/RootComponent.kt
+++ app/src/main/java/com/example/trackernew/presentation/root/RootComponent.kt
@@ -2,6 +2,7 @@ package com.example.trackernew.presentation.root
 
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
+import com.example.trackernew.presentation.add.category.AddCategoryComponent
 import com.example.trackernew.presentation.add.task.AddTaskComponent
 import com.example.trackernew.presentation.edit.EditTaskComponent
 import com.example.trackernew.presentation.tasks.TasksComponent
@@ -17,5 +18,7 @@ interface RootComponent {
         data class Tasks(val component: TasksComponent) : Child
 
         data class EditTask(val component: EditTaskComponent) : Child
+
+        data class AddCategory(val component: AddCategoryComponent) : Child
     }
 }
