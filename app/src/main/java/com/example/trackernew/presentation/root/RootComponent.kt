9ae9f19690f0ef352ce15e385a861aabded19319
stepann <stepan.snegovskoi@mail.ru>
2025-03-03 14:47:45 +0300

    Update RootComponent

--- app/src/main/java/com/example/trackernew/presentation/root/RootComponent.kt
+++ app/src/main/java/com/example/trackernew/presentation/root/RootComponent.kt
@@ -3,6 +3,7 @@ package com.example.trackernew.presentation.root
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 import com.example.trackernew.presentation.add.task.AddTaskComponent
+import com.example.trackernew.presentation.edit.EditTaskComponent
 import com.example.trackernew.presentation.tasks.TasksComponent
 
 interface RootComponent {
@@ -14,5 +15,7 @@ interface RootComponent {
         data class AddTask(val component: AddTaskComponent) : Child
 
         data class Tasks(val component: TasksComponent) : Child
+
+        data class EditTask(val component: EditTaskComponent) : Child
     }
 }
