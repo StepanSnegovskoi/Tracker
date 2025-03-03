92a26974680a0a445e0b24938b496056aee5899b
stepann <stepan.snegovskoi@mail.ru>
2025-03-03 14:44:17 +0300

    Update DefaultTasksComponent

--- app/src/main/java/com/example/trackernew/presentation/tasks/DefaultTasksComponent.kt
+++ app/src/main/java/com/example/trackernew/presentation/tasks/DefaultTasksComponent.kt
@@ -16,14 +16,22 @@ import kotlinx.coroutines.flow.onEach
 class DefaultTasksComponent @AssistedInject constructor(
     private val tasksStoreFactory: TasksStoreFactory,
     @Assisted("componentContext") componentContext: ComponentContext,
-    @Assisted("onAddClick") private val onAddClick: () -> Unit
+    @Assisted("onAddClick") private val onAddClick: () -> Unit,
+    @Assisted("onTaskLongClick") private val onTaskLongClick: () -> Unit
 ) : TasksComponent, ComponentContext by componentContext {
 
     val store = instanceKeeper.getStore { tasksStoreFactory.create() }
 
     init {
         store.labels.onEach {
-            onAddClick()
+            when(it){
+                TasksStore.Label.ClickAdd -> {
+                    onAddClick()
+                }
+                TasksStore.Label.LongClickTask -> {
+                    onTaskLongClick()
+                }
+            }
         }.launchIn(componentScope())
     }
 
@@ -34,12 +42,17 @@ class DefaultTasksComponent @AssistedInject constructor(
         store.accept(TasksStore.Intent.ClickAdd)
     }
 
+    override fun onTaskLongClicked() {
+        store.accept(TasksStore.Intent.LongClickTask)
+    }
+
     @AssistedFactory
     interface Factory {
 
         fun create(
             @Assisted("componentContext") componentContext: ComponentContext,
-            @Assisted("onAddClick") onAddClick: () -> Unit
+            @Assisted("onAddClick") onAddClick: () -> Unit,
+            @Assisted("onTaskLongClick") onTaskLongClick: () -> Unit,
         ): DefaultTasksComponent
     }
 }
