2aa061b59311edd890e2c89e0a467f883ee3fe64
stepann <stepan.snegovskoi@mail.ru>
2025-03-03 21:24:09 +0300

    Add categories in Add and Edit -TaskRepository

--- app/src/main/java/com/example/trackernew/domain/repository/EditTaskRepository.kt
+++ app/src/main/java/com/example/trackernew/domain/repository/EditTaskRepository.kt
@@ -1,8 +1,12 @@
 package com.example.trackernew.domain.repository
 
+import com.example.trackernew.domain.entity.Category
 import com.example.trackernew.domain.entity.Task
+import kotlinx.coroutines.flow.Flow
 
 interface EditTaskRepository {
+
+    val categories: Flow<List<Category>>
 
     suspend fun editTask(task: Task)
 }
