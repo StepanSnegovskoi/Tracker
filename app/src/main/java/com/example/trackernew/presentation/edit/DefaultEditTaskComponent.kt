3d3ad6a5e55cb8eb254c55a5b6a6a4cbca399062
stepann <stepan.snegovskoi@mail.ru>
2025-03-03 14:45:24 +0300

    Create DefaultEditTaskComponent

---
+++ app/src/main/java/com/example/trackernew/presentation/edit/DefaultEditTaskComponent.kt
@@ -0,0 +1,51 @@
+package com.example.trackernew.presentation.edit
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
+import com.example.trackernew.domain.entity.Task
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.StateFlow
+
+
+class DefaultEditTaskComponent @AssistedInject constructor(
+    private val storeFactory: EditTaskStoreFactory,
+    @Assisted("componentContext") componentContext: ComponentContext
+): EditTaskComponent, ComponentContext by componentContext {
+
+    private val store = instanceKeeper.getStore { storeFactory.create() }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override val model: StateFlow<EditTaskStore.State> = store.stateFlow
+
+    override fun onEditTaskClicked() {
+        store.accept(EditTaskStore.Intent.EditTaskClicked)
+    }
+
+    override fun onNameChanged(name: String) {
+        store.accept(EditTaskStore.Intent.ChangeName(name))
+    }
+
+    override fun onDescriptionChanged(description: String) {
+        store.accept(EditTaskStore.Intent.ChangeDescription(description))
+    }
+
+    override fun onCategoryChanged(category: String) {
+        store.accept(EditTaskStore.Intent.ChangeCategory(category))
+    }
+
+    override fun onDeadlineChanged(deadline: Long) {
+        store.accept(EditTaskStore.Intent.ChangeDeadline(deadline))
+    }
+
+    @AssistedFactory
+    interface Factory {
+
+        fun create(
+            @Assisted("componentContext") componentContext: ComponentContext
+        ): DefaultEditTaskComponent
+    }
+}
