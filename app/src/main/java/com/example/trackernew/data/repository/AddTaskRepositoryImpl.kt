1b095dd56c91d001b2db0813e5f25eb920c14688
stepann <stepan.snegovskoi@mail.ru>
2025-03-03 21:38:08 +0300

    Add categories in AddTaskRepositoryImpl

--- app/src/main/java/com/example/trackernew/data/repository/AddTaskRepositoryImpl.kt
+++ app/src/main/java/com/example/trackernew/data/repository/AddTaskRepositoryImpl.kt
@@ -1,16 +1,25 @@
 package com.example.trackernew.data.repository
 
+import com.example.trackernew.data.db.CategoryDao
 import com.example.trackernew.data.db.TasksDao
 import com.example.trackernew.data.mapper.toDbModel
+import com.example.trackernew.data.mapper.toEntities
+import com.example.trackernew.domain.entity.Category
 import com.example.trackernew.domain.entity.Task
 import com.example.trackernew.domain.repository.AddTaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class AddTaskRepositoryImpl @Inject constructor(
-    private val tasksDao: TasksDao
+    private val tasksDao: TasksDao,
+    private val categoryDao: CategoryDao
 ) : AddTaskRepository {
 
     override suspend fun saveTask(task: Task) {
         tasksDao.saveTask(task.toDbModel())
     }
+
+    override val categories: Flow<List<Category>> = categoryDao.getCategories()
+        .map { it.toEntities() }
 }
