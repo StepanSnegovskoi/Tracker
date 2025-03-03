aebf4a32045b4d10daa6bea5594085653c33ee40
stepann <stepan.snegovskoi@mail.ru>
2025-03-03 14:38:08 +0300

    Add editTaskMethod in dao

--- app/src/main/java/com/example/trackernew/data/db/TasksDao.kt
+++ app/src/main/java/com/example/trackernew/data/db/TasksDao.kt
@@ -2,6 +2,7 @@ package com.example.trackernew.data.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.trackernew.data.entity.TaskDbModel
 import com.example.trackernew.domain.entity.Task
@@ -10,8 +11,11 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TasksDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun saveTask(taskDbModel: TaskDbModel)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun editTask(taskDbModel: TaskDbModel)
 
     @Query("SELECT * FROM tasks")
     fun getTasks(): Flow<List<TaskDbModel>>
