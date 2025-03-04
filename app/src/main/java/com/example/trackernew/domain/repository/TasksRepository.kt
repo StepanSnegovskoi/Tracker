60c7dda7ff463f74eb2708f15c86472ac50db3e9
stepann <stepan.snegovskoi@mail.ru>
2025-03-04 21:39:52 +0300

    Add categories in TasksRepository

--- app/src/main/java/com/example/trackernew/domain/repository/TasksRepository.kt
+++ app/src/main/java/com/example/trackernew/domain/repository/TasksRepository.kt
@@ -1,9 +1,12 @@
 package com.example.trackernew.domain.repository
 
+import com.example.trackernew.domain.entity.Category
 import com.example.trackernew.domain.entity.Task
 import kotlinx.coroutines.flow.Flow
 
 interface TasksRepository {
+
+    val categories: Flow<List<Category>>
 
     val tasks: Flow<List<Task>>
 }
